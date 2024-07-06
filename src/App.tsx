28ff624cdd26295a2e09626f58be5f03697f0ed4
Cheetachu <macavity042@gmail.com>
2024-07-06 10:51:21 -0600

    Added hideHelp option.

--- src/App.tsx
+++ src/App.tsx
@@ -28,16 +28,19 @@ function App() {
     const hideSeparators =
         queryParams.get("hideSeparators")?.toLowerCase() === "true";
     const hour12 = queryParams.get("hour12")?.toLowerCase() === "true";
+    const hideHelp = queryParams.get("hideHelp")?.toLowerCase() === "true";
 
     return (
         <>
             <div id="toolbar">
-                <a
-                    href="https://github.com/cheetahchu/linear-clock?tab=readme-ov-file#readme"
-                    target="_blank"
-                >
-                    Help
-                </a>
+                {!hideHelp && (
+                    <a
+                        href="https://github.com/cheetahchu/linear-clock?tab=readme-ov-file#readme"
+                        target="_blank"
+                    >
+                        Help
+                    </a>
+                )}
             </div>
             <div className="display-container">
                 <DigitalClock date={date} hour12={hour12} />
